6b1409e69e95d16840d93911b6d3b71547e5153c
Soroush Khosravi <sorush.kh68@gmail.com>
2019-02-22 11:52:55 +0000

    The server finishes working when you type quit.

--- server.rb
+++ server.rb
@@ -2,12 +2,15 @@ require 'socket'
 
 server = TCPServer.new(2345)
 
-while true
+connected = true
+while connected == true
   socket = server.accept
 
   socket.puts "What do you say?"
 
   they_said = socket.gets.chomp
+
+  connected = false if they_said == "quit"
 
   socket.puts "You said: #{they_said}. Goodbye!"
 
