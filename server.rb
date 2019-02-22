2d012cc4720286e67dcfe8ec5dba6b0448e42453
Soroush Khosravi <sorush.kh68@gmail.com>
2019-02-22 11:49:18 +0000

    The server will not disconnect after the first user working.

--- server.rb
+++ server.rb
@@ -2,12 +2,14 @@ require 'socket'
 
 server = TCPServer.new(2345)
 
-socket = server.accept
+while true
+  socket = server.accept
 
-socket.puts "What do you say?"
+  socket.puts "What do you say?"
 
-they_said = socket.gets.chomp
+  they_said = socket.gets.chomp
 
-socket.puts "You said: #{they_said}. Goodbye!"
+  socket.puts "You said: #{they_said}. Goodbye!"
 
-socket.close
+  socket.close
+end
